c4ea20776d1716f0848812d83c1b858ec917666b
sdpaisa <sdpaisa@gmail.com>
2024-01-16 14:18:12 -0500

    added @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosModule } from './todos/todos.module';
 import { StoreModule } from '@ngrx/store';
 import { todosReducer } from './todos/state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, TodosModule, StoreModule.forRoot({
     todosState: todosReducer,
-  })],
+  }), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })],
   providers: [],
   bootstrap: [AppComponent],
 })
